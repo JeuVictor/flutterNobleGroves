4ad76b2b97d43b251a3e61b733f638671cecb7a8
JeuVictor <jeu.victor14@gmail.com>
2023-10-19 17:42:23 -0300

    corrigindo arquivos

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_noblegroves/firebase_options.dart';
 import 'package:flutter_noblegroves/telas/homeTela.dart';
 import 'package:flutter_noblegroves/telas/loginTela.dart';
 import 'package:flutter_noblegroves/telas/registroTela.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
